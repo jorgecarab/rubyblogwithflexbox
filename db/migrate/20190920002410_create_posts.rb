c8c014f43b51d90ac8aabf83eb1bc07414f7a392
jorgeluis21 <jcarabalica@gmail.com>
2019-09-19 20:03:25 -0500

    create two controllers. home: allows to show the posts post:accepting new posts and saving submitted posts

--- db/migrate/20190920002410_create_posts.rb
+++ db/migrate/20190920002410_create_posts.rb
@@ -1,8 +1,18 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
-  def change
+  def up
     create_table :posts do |t|
+
+      t.text :title
+      t.text :body
+      t.text :author
+      t.text :media
 
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :posts
+
   end
 end
