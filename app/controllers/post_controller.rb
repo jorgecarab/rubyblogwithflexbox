f268ecd3234ec3eaa436fef9e80cc30e92e9d676
jorgeluis21 <jcarabalica@gmail.com>
2019-09-19 21:27:49 -0500

    updated post controller to receive images and data from form  in view/post/create, I also added cloudinary in order to save the image

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -3,5 +3,12 @@ class PostController < ApplicationController
   end
 
   def store
+
+    #uploads picture to cloud
+    @image = Cloudinary::Uploader.upload(params[:media])
+    @post = Post.new({:title => params[:title], :body => params[:body], :author => params[:author], :media => @image['secure_url']})
+    @post.save
+    #create a post object and saves to the database
+
   end
 end
