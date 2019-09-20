5c4e965ee10e057b999f892827d10c80c7f61b0d
jorgeluis21 <jcarabalica@gmail.com>
2019-09-20 11:58:17 -0500

    added clearance forms layout and also user can see from homepage their lastes post and all the posts from the community

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
+  before_action :require_login, only: [:userpost]
   def index
     @posts =Post.all.order("created_at DESC")
+    if signed_in?
+    @myposts = Post.where({:user_id=> current_user.id})
+    end
+
   end
+
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'post/create'
   post 'post/store', only: [:store]
-  get 'home/index'
+  get 'home/index',only: [:userpost]
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
