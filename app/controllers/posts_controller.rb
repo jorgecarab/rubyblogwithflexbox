b9c72dd7bc58eb84cc7d2e02ae05dd4df7fd86a6
jorgeluis21 <jcarabalica@gmail.com>
2019-09-29 14:03:33 -0500

    added edit functionality to the blog, now user can edit their artwork

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :require_login, only: [:store, :destroy, :delete]
+  before_action :require_login, only: [:store, :destroy, :delete, :edit]
 
   def index
     @posts =Post.all.order("created_at DESC")
@@ -13,26 +13,50 @@ class PostsController < ApplicationController
     @post= Post.find(params[:id]) #encontrar  id  actual del post.
   end
 
+
+
   def create
+
   end
+
 
   def store
 
     #uploads picture to cloud
 
-    @image = Cloudinary::Uploader.upload(params[:media])
-    @post = Post.new({:user_id =>current_user.id, :title => params[:title], :body => params[:body], :author => current_user.name,:public_key =>@image['public_id'], :media => @image['secure_url']})
+    @image = Cloudinary::Uploader.upload(params[:post][:media])
+    @post = Post.new({:user_id =>current_user.id, :title => params[:post][:title], :body => params[:post][:body], :author => current_user.name,:public_key =>@image['public_id'], :media => @image['secure_url']})
 
-    
-     if @post.save
+    if @post.save
+
     #create a post object and saves to the database
-
-
     else
       render('create')
 
     end
   end
+
+  def edit
+  @post= Post.find(params[:id]) #encontrar  id  actual del post.
+
+
+   end
+
+  def update
+    @post= Post.find(params[:id]) #encontrar  id  actual del post.
+
+    @image = Cloudinary::Uploader.upload(params[:post][:media])
+
+   if @post.update_attributes({:title => params[:post][:title], :body => params[:post][:body],:public_key =>@image['public_id'], :media => @image['secure_url'] } )
+     redirect_to({ :controller => 'posts', :action => 'show', :id =>@post.id})
+
+   else
+
+     render ('create')
+   end
+end
+
+
 
   def delete
 
@@ -48,5 +72,9 @@ class PostsController < ApplicationController
   @post.destroy
 
   end
+
+
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   get 'posts/create'
   get 'posts/delete'
-  post 'posts/destroy', only: [:destroy]
-  post 'posts/store', only: [:store]
   get 'posts/index',only: [:userpost]
   get '/posts/:id', to: 'posts#show'
+  get '/posts/edit/:id', to: 'posts#edit'
+  post 'posts/destroy', only: [:destroy]
+  post '/posts/store', action: :store, controller: 'posts'
+  post '/posts/update/:id', action: :update, controller: 'posts'
+
   root to: 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
